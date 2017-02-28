04cc5df974bb111177ea18e89ca0f4ce51e04909
dav <dav2070@web.de>
2017-02-28 22:14:34 +0000

    Add form for adding a entry and listing of entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,6 +5,28 @@ class EntriesController < ApplicationController
     end
     
     def new
+        @entry = Entry.new
+    end
+    
+    def create
+        @entry = Entry.new(entry_params)
+        @entry.user_id = current_user
         
+        if @entry.save
+            flash[:success] = "Eintrag erfolgreich gespeichert."
+            redirect_to root_path
+        else
+           flash[:danger] = "Es gab einen Fehler beim Speichern!"
+           render :new
+        end
+    end
+    
+    def show
+       @entry = Entry.find(params[:id])
+    end
+    
+    private
+    def entry_params
+        params.require(:entry).permit(:title, :text, :date)
     end
 end
